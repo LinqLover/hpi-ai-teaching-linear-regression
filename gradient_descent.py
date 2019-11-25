f7f4ae2691d6b33beb20dc3169089b55060fa936
LinqLover <christoph.thiede@student.hpi.de>
2019-11-25 20:59:37 +0100

    Update template

--- gradient_descent.py
+++ gradient_descent.py
@@ -7,28 +7,34 @@ with open('samples.json') as samples_file:
 
 
 from statistics import mean
-from math import sqrt
 import random
 
 def hw(x):
 	return w1 * x + w0
 
-def loss():
-	raise NotImplementedError() # TODO: Compute variance here
+def stderr(samples, w1, w0):
+	raise NotImplementedError() # TODO: Compute standard error here
+
+# TODO: Compute gradient here
+def gradient_w0(samples, w1, w0):
+	raise NotImplementedError()
+def gradient_w1(samples, w1, w0):
+	raise NotImplementedError()
 
 w1, w0 = 0, 0 # TODO: Initialize weights here
 learning_rate = 0 # TODO: Initialize learning rate here
 
 iteration = 0
 steps_per_iteration = 50
-while False: # TODO: Check for convergence here
+while stderr(samples, w1, w0) > 123: # TODO: Insert convergence treshold here
 	iteration = iteration + steps_per_iteration
-	print(f"Iteration {iteration}, loss = {loss()}")
+	print(f"Iteration {iteration}, w = ({w0:.2f}, {w1:.2f}), stderr = {stderr(samples, w1, w0):.2f}")
 	
 	for i in range(0, steps_per_iteration):
-		raise NotImplementedError() # TODO: Adjust weights here
+		# TODO: Update weights from gradient here
+		pass
 
-print(f"(w0, w1) = {(w0, w1)}")
 print("Samples:")
+print("x, y, hw(x)")
 for sample in samples:
-	print((sample[0], sample[1], hw(sample[0])))
+	print(f"{sample[0]}, {sample[1]}, {hw(sample[0]):.2f}")
